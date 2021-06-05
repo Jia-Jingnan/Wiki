114867db2bf25342af9e9fbad230e9cd594491ff
Jia-Jingnan <875480307@qq.com>
2021-06-06 00:23:53 +0800

    0606-初始化Wiki项目

---
+++ src/test/java/com/lilith/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilith.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
