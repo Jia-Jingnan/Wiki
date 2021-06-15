e3779a6e94802eb5c2ed0584d3c7067e83a195dd
Jia-Jingnan <875480307@qq.com>
2021-06-15 20:48:22 +0800

    0615-增加注解，所有的单元测试类都继承该类

--- src/test/java/com/lilith/wiki/WikiApplicationTests.java
+++ src/test/java/com/lilith/wiki/WikiApplicationTests.java
@@ -1,13 +1,16 @@
 package com.lilith.wiki;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class WikiApplicationTests {
+@RunWith(SpringRunner.class)
+public class WikiApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
